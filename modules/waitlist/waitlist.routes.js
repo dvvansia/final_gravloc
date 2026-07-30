3b27b7d752b65f1a0e6910f2fa9350d1b7cadcc3
dvvansia <116154883+dvvansia@users.noreply.github.com>
2026-07-30 15:45:54 +0200

    Add GET route for waitlist entries
    Added GET route to retrieve all waitlist entries.

--- modules/waitlist/waitlist.routes.js
+++ modules/waitlist/waitlist.routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const waitlistController = require("./waitlist.controller");
 
+// POST - Add email to waitlist
 router.post("/", waitlistController.addToWaitlist);
+
+// GET - View all waitlist entries
+router.get("/", waitlistController.getWaitlist);
 
 module.exports = router;
