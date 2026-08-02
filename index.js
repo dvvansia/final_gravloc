6a1af16fc93b34abfb92025bc58fb38c85363868
dvvansia <116154883+dvvansia@users.noreply.github.com>
2026-08-02 23:55:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config(); // ✅ Must be first!
 
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const app = require("./app");
 const { sequelize } = require("./models");
 
