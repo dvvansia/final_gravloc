3166d66f96c85111256fa5ece717347f85c8857c
dvvansia <116154883+dvvansia@users.noreply.github.com>
2026-08-02 23:22:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ async function startServer() {
   try {
     await sequelize.authenticate();
     console.log("✅ Database Connected");
+    
+    await sequelize.sync({ alter: true });
+    console.log("✅ Database Synced");
+    
     app.listen(PORT, () => {
       console.log(`🚀 Server running on ${PORT}`);
     });
