4639488e13fc642f3dc065fb72c6d985c5a84ec0
dvvansia <116154883+dvvansia@users.noreply.github.com>
2026-08-02 22:56:23 +0200

    Enhance Sequelize configuration with DATABASE_URL
    Added support for DATABASE_URL environment variable for Sequelize connection.

--- models/index.js
+++ models/index.js
@@ -10,10 +10,29 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
+
+if (process.env.DATABASE_URL) {
+  console.log("✅ models/index.js using DATABASE_URL");
+
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
 fs
