9e9eed4fb4ed77263174e417c8b4a36e40b5c8e3
dvvansia <116154883+dvvansia@users.noreply.github.com>
2026-08-06 09:06:51 +0200

    Update waitlist.routes.js

--- modules/user-waitlist/waitlist.routes.js
+++ modules/user-waitlist/waitlist.routes.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const waitlistController = require("./waitlist.controller");
 
-// POST - Add email to waitlist
 router.post("/", waitlistController.addToWaitlist);
-
-// GET - View all waitlist entries
 router.get("/", waitlistController.getWaitlist);
+router.delete("/:id", waitlistController.deleteWaitlistEntry); // <-- Add this line
 
 module.exports = router;
